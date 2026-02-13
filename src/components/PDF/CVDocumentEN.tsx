6445ca9d9bf2e27f57d535a4cd7acac5d01379a1
Erick Bores <e.bores.i@outlook.com>
2026-02-13 00:33:28 -0600

    fix: Register Roboto Italic font to resolve PDF generation error

--- src/components/PDF/CVDocumentEN.tsx
+++ src/components/PDF/CVDocumentEN.tsx
@@ -8,6 +8,7 @@ Font.register({
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 400 },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf', fontWeight: 500 },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 700 },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-italic-webfont.ttf', fontStyle: 'italic' },
     ],
 });
 
